53a4437196f2a0f4f567cc73056d7b3571f729fc
morimorig3 <mori.nto@outlook.jp>
2024-10-14 16:30:53 +0900

    initial commit

---
+++ src/main/java/com/morimorig3/learningSpringApp/LearningSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.morimorig3.learningSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morimorig3/learningSpringApp/LearningSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morimorig3.learningSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
